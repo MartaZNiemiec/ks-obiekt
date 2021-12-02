17d2bdf207724162bf14a0f4fcd61e4aa37cc0b1
Marta Gonsiorowska <marta.niemiec.programista@gmail.com>
2021-12-02 19:36:21 +0100

    Added load user to vector at start

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
-    ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
 
     return 0;
 }
